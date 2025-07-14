8733095bde23564d990cafcf07e086f5df47b0f5
Emarson-s <emarson.s@mavens-i.com>
2025-07-14 19:27:57 +0530

    First Commit

---
+++ src/main/java/com/example/IKSANA_API_AUTOMATION/IksanaApiAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.example.IKSANA_API_AUTOMATION;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IksanaApiAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IksanaApiAutomationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/IKSANA_API_AUTOMATION/IksanaApiAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.IKSANA_API_AUTOMATION;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IksanaApiAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
